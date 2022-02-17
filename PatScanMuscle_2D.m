8d091efa12653ddcd0fed7fd41fbdd7be406d04e
Benoit Dehapiot <b.dehapiot@gmail.com>
2022-02-17 14:12:13 +0100

    update

--- PatScanMuscle_2D.m
+++ PatScanMuscle_2D.m
@@ -8,36 +8,37 @@ clearvars
 
 %% Inputs
 
-RawName = 'MAX_Trial8_D12_C1(488-TTNrb)_C2(633-MHCall)_C3(DAPI)_C4(568-Phalloidin)_100x_01_stiched_med';
+RawName = 'MAX_Trial8_D07_C1(488-TTNrb)_C2(633-MHCall)_C3(DAPI)_C4(568-Phalloidin)_100x_01_stiched_lite';
 RootPath = 'C:\Datas\3-GitHub_BDehapiot\PatScanMuscle_2D\data';
 pixSize = 0.0830266; % pixel size (µm)
 nChannel = 1; % select channel to process
 
 %% Parameters 
 
-% Tophat filtering
-TophatSize = 6; % disk size for tophat filtering (pixels)
-
 % Mask  (obtained from BGSub)
-mThresh = 125; % lower threshold for Mask (A.U.)
+Mask_Thresh = 125; % lower threshold for Mask (A.U.)
 
 % ROIsMask (obtained from Mask)
-rSize = 10; % ROIs size for ROIsMask (pixels)
-rThresh = 0.25; % lower threshold for ROIsMask (A.U.)
-rMinSize = 100; % min size for ROIsMask's objects (pixels)
-rMaxSize = 50000; % max size for ROIsMask's objects (pixels)
+ROI_Size = 10; % ROIs size for ROIsMask (pixels)
+ROI_Thresh = 0.25; % lower threshold for ROIsMask (A.U.)
+ROI_MinSize = 100; % min size for ROIsMask's objects (pixels)
+ROI_MaxSize = 50000; % max size for ROIsMask's objects (pixels)
 
-Pad1 = 10; % Orientations
-rPad1 = rSize+(rSize*(Pad1-1))*2;
-Pad2 = 5; % Correlations (Pad1 must be > to Pad2)
-rPad2 = rSize+(rSize*(Pad2-1))*2;
+% Tophat filtering
+Tophat_Size = 6; % disk size for tophat filtering (pixels)
 
 % Steerable Filter
-sfOrder = 2; sfSigma = 3;
+Steerable_Order = 2; Steerable_Sigma = 2;
+
+% Padding
+Pad1 = 8; % Orientations
+ROI_Pad1 = ROI_Size+(ROI_Size*(Pad1-1))*2;
+Pad2 = 4; % Correlations (Pad1 must be > to Pad2)
+ROI_Pad2 = ROI_Size+(ROI_Size*(Pad2-1))*2;
 
 % Valid peaks
-minProm = 0.10; % min prominence for pattern recognition
-minLoc = 0; maxLoc = 100; % min/max size for pattern recognition (µm)
+min_Prom = 0.10; % min prominence for pattern recognition
+min_Loc = 0; max_Loc = 100; % min/max size for pattern recognition (µm)
     
 %% Initialize
 
@@ -63,60 +64,60 @@ while choice > 0
 % Process .................................................................
 
     % Crop Raw
-    nGridY = floor(nY/rSize);
-    nGridX = floor(nX/rSize);
-    Raw(nGridY*rSize+1:end,:) = [];
-    Raw(:,nGridX*rSize+1:end) = [];
+    nGridY = floor(nY/ROI_Size);
+    nGridX = floor(nX/ROI_Size);
+    Raw(nGridY*ROI_Size+1:end,:) = [];
+    Raw(:,nGridX*ROI_Size+1:end) = [];
     nYCrop = size(Raw,1);
     nXCrop = size(Raw,2);
     
     % Substract background
-    BGSub = Raw-imgaussfilt(Raw,20); % Gaussian blur #1 
-    BGSub = imgaussfilt(BGSub,1); % Gaussian blur #2
+    Raw_BGSub = Raw-imgaussfilt(Raw,20); % Gaussian blur #1 
+    Raw_BGSub = imgaussfilt(Raw_BGSub,1); % Gaussian blur #2
 
     % Create Mask
-    Mask = BGSub;
-    Mask(Mask<mThresh) = 0;
-    Mask(Mask>=mThresh) = 1;
+    Raw_Mask = Raw_BGSub;
+    Raw_Mask(Raw_Mask<Mask_Thresh) = 0;
+    Raw_Mask(Raw_Mask>=Mask_Thresh) = 1;
 
     % Create ROIsMask
-    ROIsMask = zeros(nGridY,nGridX);
+    Raw_ROIsMask = zeros(nGridY,nGridX);
     for i=1:nGridY
         for j=1:nGridX
-            temp = mean(Mask(rSize*i-(rSize-1):rSize*i,rSize*j-(rSize-1):rSize*j));
-            if mean(temp(:)) > rThresh
+            temp = mean(Raw_Mask(ROI_Size*i-(ROI_Size-1):ROI_Size*i,ROI_Size*j-(ROI_Size-1):ROI_Size*j));
+            if mean(temp(:)) > ROI_Thresh
                 if i >= Pad1 && i <= nGridY-(Pad1-1) && j >= Pad1 && j <= nGridX-(Pad1-1)
-                    ROIsMask(i,j) = 1;
+                    Raw_ROIsMask(i,j) = 1;
                 end
             end
         end
     end
 
     % Apply size filter (ROIsMask)
-    ROIsMask = logical(ROIsMask);
-    ROIsMask = bwareafilt(ROIsMask,[rMinSize rMaxSize]);
+    Raw_ROIsMask = logical(Raw_ROIsMask);
+    Raw_ROIsMask = bwareafilt(Raw_ROIsMask,[ROI_MinSize ROI_MaxSize]);
     
 % Display .................................................................
 
     % BGSub
     subplot(3,1,1) 
-    imshow(BGSub,[qLow qHigh])
+    imshow(Raw_BGSub,[qLow qHigh])
     title('BGSub')
 
     % Mask
     subplot(3,1,2) 
-    imshow(Mask,[0 1])
+    imshow(Raw_Mask,[0 1])
     title(strcat(...
-        'Mask (thresh. =',{' '},num2str(mThresh),')'));
+        'Mask (thresh. =',{' '},num2str(Mask_Thresh),')'));
     
     % ROIsMask
     subplot(3,1,3) 
-    imshow(ROIsMask,[0 1])
+    imshow(Raw_ROIsMask,[0 1])
     title(strcat(...
-        'ROIsMask (size = ',{' '},num2str(rSize),{' '},'pix. ;',...
-        {' '},'thresh =',{' '},num2str(rThresh),{' '},';',...
-        {' '},'min size =',{' '},num2str(rMinSize),{' '},'pix. ;',...
-        {' '},'max size =',{' '},num2str(rMaxSize),{' '},'pix.)'));
+        'ROIsMask (ROIs size = ',{' '},num2str(ROI_Size),{' '},'pix. ;',...
+        {' '},'ROIs thresh. =',{' '},num2str(ROI_Thresh),{' '},';',...         
+        {' '},'min size =',{' '},num2str(ROI_MinSize),{' '},'nROIs ;',...
+        {' '},'max size =',{' '},num2str(ROI_MaxSize),{' '},'nROIs)'));
     
     set(gcf, 'Units', 'Normalized', 'OuterPosition', [0 0 1 1]);
     
@@ -135,29 +136,29 @@ while choice > 0
     if choice == 1
 
         prompt = {
-            'Mask thresh. :',...
-            'ROIs size :',...
-            'ROIs thresh. :',...
-            'ROIs min. size :',...
-            'ROIs max. size :'
+            'Mask_Thresh :',...
+            'ROI_Size :',...
+            'ROI_Thresh :',...
+            'ROI_MinSize :',...
+            'ROI_MaxSize :'
             };
 
         definput = {
-            num2str(mThresh),...
-            num2str(rSize),....
-            num2str(rThresh),...
-            num2str(rMinSize),...
-            num2str(rMaxSize)
+            num2str(Mask_Thresh),...
+            num2str(ROI_Size),....
+            num2str(ROI_Thresh),...
+            num2str(ROI_MinSize),...
+            num2str(ROI_MaxSize)
             };
         
         dlgtitle = 'Input'; dims = 1;
         answer = str2double(inputdlg(prompt,dlgtitle,dims,definput));
         
-        mThresh = answer(1,1); 
-        rSize = answer(2,1); 
-        rThresh = answer(3,1); 
-        rMinSize = answer(4,1); 
-        rMaxSize = answer(5,1);
+        Mask_Thresh = answer(1,1); 
+        ROI_Size = answer(2,1); 
+        ROI_Thresh = answer(3,1); 
+        ROI_MinSize = answer(4,1); 
+        ROI_MaxSize = answer(5,1);
         
         close
     end
@@ -177,7 +178,7 @@ while choice > 0
 % Process .................................................................    
     
     % Tophat filtering
-    Tophat = imtophat(Raw,strel('disk',TophatSize));
+    Raw_Tophat = imtophat(Raw,strel('disk',Tophat_Size));
     
 % Display .................................................................
 
@@ -188,9 +189,9 @@ while choice > 0
 
     % Tophat
     subplot(2,1,2) 
-    imshow(Tophat,[qLow qHigh])
+    imshow(Raw_Tophat,[qLow qHigh])
     title(strcat(...
-        'Tophat (size =',{' '},num2str(TophatSize),{' '},'pix.)'));
+        'Tophat (size =',{' '},num2str(Tophat_Size),{' '},'pix.)'));
     
     set(gcf, 'Units', 'Normalized', 'OuterPosition', [0 0 1 1]);
     
@@ -208,14 +209,14 @@ while choice > 0
 
     if choice == 1
         
-        prompt = {'Tophat size :'};
+        prompt = {'Tophat_Size :'};
         
-        definput = {num2str(TophatSize)};
+        definput = {num2str(Tophat_Size)};
         
         dlgtitle = 'Input'; dims = 1;
         answer = str2double(inputdlg(prompt,dlgtitle,dims,definput));
         
-        TophatSize = answer(1,1);
+        Tophat_Size = answer(1,1);
         
         close
     end
@@ -236,8 +237,8 @@ while choice > 0
 
     % Steerable Filter
     RawRSize = double(imresize(Raw,[nGridY nGridX],'nearest'));
-    MaskRSize = double(imresize(Mask,[nGridY nGridX],'nearest'));
-    [res,~,~,rot] = steerableDetector(RawRSize,sfOrder,sfSigma,180);
+    MaskRSize = double(imresize(Raw_Mask,[nGridY nGridX],'nearest'));
+    [res,~,~,rot] = steerableDetector(RawRSize,Steerable_Order,Steerable_Sigma,180);
     for i=1:size(rot,3)
         temp = rot(:,:,i);
         temp(MaskRSize==0) = NaN;
@@ -248,7 +249,7 @@ while choice > 0
     AngleMap = zeros(nGridY,nGridX);
     parfor i=1:nGridY
         for j=1:nGridX
-            if ROIsMask(i,j) == 1            
+            if Raw_ROIsMask(i,j) == 1            
                 Crop = rot(i-(Pad1-1):i+(Pad1-1),j-(Pad1-1):j+(Pad1-1),:);
                 tempMax = NaN(size(Crop,3),1);
                 for k=1:size(Crop,3)
@@ -272,13 +273,12 @@ while choice > 0
     subplot(3,1,2)
     imshow(res,[min(res(:)) max(res(:))])
     title(strcat(...
-        'Steerable filter (sigma =',{' '},num2str(sfSigma),{' '},'pix.)'));
+        'Steerable filter (sigma =',{' '},num2str(Steerable_Sigma),{' '},'pix.)'));
 
     % AngleMap
     subplot(3,1,3)
     imshow(AngleMap,[0 180])
-    title(strcat(...
-        'AngleMap (Pad1 =',{' '},num2str(Pad1),{' '},'nROIs.)'));
+    title('AngleMap')
     colormap(gca, jet(256));
     colorbar(gca);
 
@@ -299,11 +299,11 @@ while choice > 0
     if choice == 1
 
         prompt = {
-            'Sigma :'
+            'Steerable_Sigma :'
             };
 
         definput = {
-            num2str(sfSigma)
+            num2str(Steerable_Sigma)
             };
         
         dlgtitle = 'Input'; dims = 1;
@@ -327,11 +327,11 @@ MergedData = cell(nGridY,nGridX);
 parfor i=1:nGridY
     for j=1:nGridX
         
-        if ROIsMask(i,j) == 1  
+        if Raw_ROIsMask(i,j) == 1  
 
             % Crop Data
             Angle = AngleMap(i,j);
-            Crop = Tophat(rSize*i-((rSize*Pad2)-1):rSize*i+(rSize*(Pad2-1)),rSize*j-((rSize*Pad2)-1):rSize*j+(rSize*(Pad2-1)));
+            Crop = Raw_Tophat(ROI_Size*i-((ROI_Size*Pad2)-1):ROI_Size*i+(ROI_Size*(Pad2-1)),ROI_Size*j-((ROI_Size*Pad2)-1):ROI_Size*j+(ROI_Size*(Pad2-1)));
 
             % Determine 2D Correlation
             Corr2D = normxcorr2(Crop,Crop);
@@ -340,8 +340,8 @@ parfor i=1:nGridY
             temp_AvgX = (nanmean(Corr2D_Rot(tempMid-2:tempMid+2,:),1))'; % average value on x axis
             temp = max(temp_AvgX,[],2); [~,tempMid] = max(temp); % find max corr #2 
             temp_AvgX = temp_AvgX/max(temp_AvgX); % max normalization
-            temp_AvgX1 = temp_AvgX(tempMid:tempMid+rPad2-1);
-            temp_AvgX2 = flip(temp_AvgX(tempMid-rPad2+1:tempMid));                
+            temp_AvgX1 = temp_AvgX(tempMid:tempMid+ROI_Pad2-1);
+            temp_AvgX2 = flip(temp_AvgX(tempMid-ROI_Pad2+1:tempMid));                
             Corr2D_AvgX = horzcat(temp_AvgX1,temp_AvgX2);
             Corr2D_AvgX = mean(Corr2D_AvgX,2);
 
@@ -366,50 +366,50 @@ end
 
 %% Get results
 
-choice = 1;
-while choice > 0   
+choice = 2;
+while choice == 2   
 
 % Process .................................................................
 
     % Merge "All" data
-    All_PeaksInfo = NaN(0,0);
-    All_Corr2D_AvgX = NaN(0,0);
-    PromMap = zeros(nGridY,nGridX);
+    PeaksInfo_All = NaN(0,0);
+    Corr2D_AvgX_All = NaN(0,0);
+    Map_Prom = zeros(nGridY,nGridX);
     for i=1:nGridY
         for j=1:nGridX
-            if ROIsMask(i,j) == 1
-                All_PeaksInfo = vertcat(All_PeaksInfo,MergedData{i,j}{4,1});
-                All_Corr2D_AvgX(:,end+1) = MergedData{i,j}{3,1};
-                PromMap(i,j) = MergedData{i,j}{4,1}(1,6);
+            if Raw_ROIsMask(i,j) == 1
+                PeaksInfo_All = vertcat(PeaksInfo_All,MergedData{i,j}{4,1});
+                Corr2D_AvgX_All(:,end+1) = MergedData{i,j}{3,1};
+                Map_Prom(i,j) = MergedData{i,j}{4,1}(1,6);
             end
         end
     end
 
     % Get "Valid" data          
-    Valid_PeaksInfo = NaN(0,0);
-    Valid_Corr2D_AvgX = NaN(0,0);
-    ValidMap = zeros(nGridY,nGridX);
-    for i=1:size(All_PeaksInfo,1)
-        tempY = All_PeaksInfo(i,1);
-        tempX = All_PeaksInfo(i,2);
-        tempProm = All_PeaksInfo(i,6);
-        tempLoc = All_PeaksInfo(i,4);
-        if tempProm >= minProm
-            if tempLoc >= minLoc && tempLoc <= maxLoc
-                Valid_PeaksInfo(end+1,:) = All_PeaksInfo(i,:);
-                Valid_Corr2D_AvgX(:,end+1) = All_Corr2D_AvgX(:,i);
-                ValidMap(tempY,tempX) = 1;
+    PeaksInfo_Valid = NaN(0,0);
+    Corr2D_AvgX_Valid = NaN(0,0);
+    Map_Valid = zeros(nGridY,nGridX);
+    for i=1:size(PeaksInfo_All,1)
+        tempY = PeaksInfo_All(i,1);
+        tempX = PeaksInfo_All(i,2);
+        tempProm = PeaksInfo_All(i,6);
+        tempLoc = PeaksInfo_All(i,4);
+        if tempProm >= min_Prom
+            if tempLoc >= min_Loc && tempLoc <= max_Loc
+                PeaksInfo_Valid(end+1,:) = PeaksInfo_All(i,:);
+                Corr2D_AvgX_Valid(:,end+1) = Corr2D_AvgX_All(:,i);
+                Map_Valid(tempY,tempX) = 1;
             end
         end
     end
 
     % Get Avg Corr2D_AvgX
-    Avg_Corr2D_AvgX(:,1) = (0:rPad2-1)*pixSize; % distance (µm)
-    Avg_Corr2D_AvgX(:,2) = mean(All_Corr2D_AvgX,2); % All 
-    Avg_Corr2D_AvgX(:,3) = std(All_Corr2D_AvgX,0,2); % All S.D.
-    if ~isempty(Valid_Corr2D_AvgX)
-        Avg_Corr2D_AvgX(:,4) = mean(Valid_Corr2D_AvgX,2); % Valid
-        Avg_Corr2D_AvgX(:,5) = std(Valid_Corr2D_AvgX,0,2); % Valid S.D.
+    Corr2D_AvgX_Avg(:,1) = (0:ROI_Pad2-1)*pixSize; % distance (µm)
+    Corr2D_AvgX_Avg(:,2) = mean(Corr2D_AvgX_All,2); % All 
+    Corr2D_AvgX_Avg(:,3) = std(Corr2D_AvgX_All,0,2); % All S.D.
+    if ~isempty(Corr2D_AvgX_Valid)
+        Corr2D_AvgX_Avg(:,4) = mean(Corr2D_AvgX_Valid,2); % Valid
+        Corr2D_AvgX_Avg(:,5) = std(Corr2D_AvgX_Valid,0,2); % Valid S.D.
     end
     
 % Display .................................................................
@@ -417,50 +417,50 @@ while choice > 0
     % Raw
     subplot(3,3,1:3)
     tempRaw = Raw;
-    tempValidMap = imresize(ValidMap,[nYCrop nXCrop],'nearest');
+    tempValidMap = imresize(Map_Valid,[nYCrop nXCrop],'nearest');
     tempValidMap = bwmorph(logical(tempValidMap),'remove');
     tempValidMap = bwmorph(tempValidMap,'dilate');
     tempRaw(tempValidMap==1) = max(Raw(:));
     imshow(tempRaw,[qLow qHigh])
 	title(strcat(...
-        'ValidMap (minProm =',{' '},num2str(minProm),{' '},';',...
-        {' '},'minLoc =',{' '},num2str(minLoc),{' '},';',...
-        {' '},'maxLoc =',{' '},num2str(maxLoc),{' '},')'));
+        'MapValid (min. prominence =',{' '},num2str(min_Prom),{' '},';',...
+        {' '},'min. localization =',{' '},num2str(min_Loc),{' '},';',...
+        {' '},'max. localization =',{' '},num2str(max_Loc),{' '},')'));
 
     % PromMap
     subplot(3,3,4:6)
-    imshow(imresize(PromMap,[nYCrop nXCrop],'nearest'),[min(PromMap(:)) max(PromMap(:))])
-    title('PromMap')
+    imshow(imresize(Map_Prom,[nYCrop nXCrop],'nearest'),[min(Map_Prom(:)) max(Map_Prom(:))])
+    title('MapProm')
     colormap(gca, jet(256));
     colorbar(gca);
 
     % Avg_Corr2D_AvgX (All & Valid)
     subplot(3,3,7)
-    plot(Avg_Corr2D_AvgX(:,1), Avg_Corr2D_AvgX(:,2))
+    plot(Corr2D_AvgX_Avg(:,1), Corr2D_AvgX_Avg(:,2))
     hold on
-    plot(Avg_Corr2D_AvgX(:,1), Avg_Corr2D_AvgX(:,4))
+    plot(Corr2D_AvgX_Avg(:,1), Corr2D_AvgX_Avg(:,4))
     xlabel('Distance (µm)') 
     ylabel('Correlation')
     legend('All','Valid')
-    xlim([0 ceil(max(Avg_Corr2D_AvgX(:,1)))])
+    xlim([0 ceil(max(Corr2D_AvgX_Avg(:,1)))])
     pbaspect([1 1 1])
     title('Avg. Corr2D')
 
     % Locs distribution ('All') 
     subplot(3,3,8)
-    histogram(All_PeaksInfo(:,4),50)
+    histogram(PeaksInfo_All(:,4),50)
     xlabel('Distance (µm)') 
     ylabel('Number of peaks')
-    xlim([0 ceil(max(Avg_Corr2D_AvgX(:,1)))])
+    xlim([0 ceil(max(Corr2D_AvgX_Avg(:,1)))])
     pbaspect([1 1 1])
     title('Main peak localization (All)')
 
     % Locs distribution ('Valid')  
     subplot(3,3,9)
-    histogram(Valid_PeaksInfo(:,4),50)
+    histogram(PeaksInfo_Valid(:,4),50)
     xlabel('Distance (µm)') 
     ylabel('Number of peaks')
-    xlim([0 ceil(max(Avg_Corr2D_AvgX(:,1)))])
+    xlim([0 ceil(max(Corr2D_AvgX_Avg(:,1)))])
     pbaspect([1 1 1])
     title('Main peak localization (Valid)')
 
@@ -470,39 +470,41 @@ while choice > 0
     
     choice = questdlg('What next?', ...
         'Menu', ...
-        'Modify Parameters','Proceed','Proceed');
+        'Modify Parameters','Terminate','Terminate & Save','Terminate & Save');
     switch choice
         case 'Modify Parameters'
+            choice = 2;
+        case 'Terminate'
             choice = 1;
-        case 'Proceed'
+        case 'Terminate & Save'
             choice = 0;
     end
 
-    if choice == 1
+    if choice == 2
 
         prompt = {
-            'minProm :',...
-            'minLoc :',...
-            'maxLoc :'
+            'min_Prom :',...
+            'min_Loc :',...
+            'max_Loc :'
             };
 
         definput = {
-            num2str(minProm),...
-            num2str(minLoc),....
-            num2str(maxLoc)
+            num2str(min_Prom),...
+            num2str(min_Loc),....
+            num2str(max_Loc)
             };
         
         dlgtitle = 'Input'; dims = 1;
         answer = str2double(inputdlg(prompt,dlgtitle,dims,definput));
         
-        minProm = answer(1,1); 
-        minLoc = answer(2,1); 
-        maxLoc = answer(3,1); 
+        min_Prom = answer(1,1); 
+        min_Loc = answer(2,1); 
+        max_Loc = answer(3,1); 
         
         close
     end
 
-    if choice == 0
+    if choice < 2
         
         close
     end
@@ -511,13 +513,46 @@ end
 
 %% Format and save
 
-% Clear vars ..............................................................
-
 % Format outputs ..........................................................
 
-All_PeaksInfo = array2table(All_PeaksInfo,'VariableNames',...
-    {'GridY','GridX','pks','loc','width','prom','valid'});
-Valid_PeaksInfo = array2table(Valid_PeaksInfo,'VariableNames',...
-    {'GridY','GridX','pks','loc','width','prom','valid'});
-Avg_Corr2D_AvgX = array2table(Avg_Corr2D_AvgX,'VariableNames',...
-    {'µm','All','Valid','AllSD','ValidSD'});
+% Parameters
+Parameters = vertcat(...
+    Mask_Thresh,...
+    ROI_Size,ROI_Thresh,ROI_MinSize,ROI_MaxSize,...
+    Tophat_Size,...
+    Steerable_Order,Steerable_Sigma,...
+    Pad1, ROI_Pad1, Pad2, ROI_Pad2,...
+    min_Prom, min_Loc, max_Loc);
+
+Parameters = array2table(Parameters,'RowNames',...
+    {'Mask_Thresh',...
+    'ROI_Size','ROI_Thresh','ROI_MinSize','ROI_MaxSize',...
+    'Tophat_Size',...
+    'Steerable_Order','Steerable_Sigma',...
+    'Pad1', 'ROI_Pad1', 'Pad2', 'ROI_Pad2',...
+    'min_Prom', 'min_Loc', 'max_Loc'});
+
+PeaksInfo_All = array2table(PeaksInfo_All,'VariableNames',...
+    {'GridY','GridX','pks','loc','width','prom'});
+PeaksInfo_Valid = array2table(PeaksInfo_Valid,'VariableNames',...
+    {'GridY','GridX','pks','loc','width','prom'});
+Corr2D_AvgX_Avg = array2table(Corr2D_AvgX_Avg,'VariableNames',...
+    {'Distance','All','Valid','AllSD','ValidSD'});
+
+% Clearvars ...............................................................
+
+clearvars -except...
+    RootPath RawName choice...
+    Raw Raw_BGSub Raw_Mask Raw_ROIsMask Raw_Tophat...
+    MergedData... 
+    Parameters...
+    Corr2D_AvgX_All PeaksInfo_All...
+    Corr2D_AvgX_Valid PeaksInfo_Valid...
+    Corr2D_AvgX_Avg...
+    Map_Prom Map_Valid
+
+% Save ....................................................................
+if choice == 0
+    save(strcat(RootPath,'/temp/',RawName,'.mat'))
+end
+clear choice
